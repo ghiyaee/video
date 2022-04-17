37dddeab2555655771b60ee3052edaf699fc52f5
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-17 23:46:40 +0430

    update Detail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import './VideoDetail.css'
 
 const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>
     }
-    return <div>{video.snippet.title }</div>
+    return (
+      <div className="detail">
+            <h4 className="titel">{video.snippet.title}</h4>
+            <p>{video.snippet.description }</p>
+      </div>
+    );
 }
 export default VideoDetail;
