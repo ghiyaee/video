0a3cc640d2aa04e10007faa5d632ec74ec50bf06
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-17 23:34:14 +0430

    add Detail

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import YouTube from "../Apis/YouTube";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import './App.css'
 class App extends React.Component{
 
@@ -15,15 +16,16 @@ class App extends React.Component{
     this.setState({ videos: respons.data.items })
   }
   onVideoSelect = (video) => {
-    console.log('the app ',video);
+    this.setState({selectedVideo:video});
   
   }
     render() {
         return (
           <div className="container">
             <SearchBar onSubmit={this.onSubmitValue} />
+            <VideoDetail video ={this. state.selectedVideo} />
             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-            
+             
           </div>
         );
     }
