33841d9a1d6558f43057cb09fe5fa02fd6007cdd
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-04-17 15:19:08 +0430

    React Video

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './SearchBar.css'
+class SearchBar extends React.Component{
+    state = { term: '' }
+    onSubmatHandel = (e) => {
+        e.preventDefault()
+    }
+    onChangeHandel = (e) => {
+        this.setState({ term: e.target.value });
+        console.log(this.state.term);
+    }
+    render() {
+        return (
+            <div className="search-bar">
+                <form action="" className="frm" onSubmit={this.onSubmatHandel} >
+                    <div className="field">
+                        <label className="lbl">Video Search</label>
+                        <input type="text" className="input" onChange={this.onChangeHandel} value={this.state.term }/>
+                    </div>
+                </form>
+              </div>
+        )
+    }
+}
+export default SearchBar;
